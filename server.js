619db7dcc87cbb49b637f09f5c73a80c0fe259d5
syltur <sylwia.tur99@gmail.com>
2023-04-28 15:33:57 +0200

    add files

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/user', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/views/user_login.html'));
+});
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get(['/', '/home'], (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, ('/views/404.html')));
+})
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
